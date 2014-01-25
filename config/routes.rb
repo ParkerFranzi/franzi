d1a396d404d4cd13835a06e22163047fde7cc658
Parker Franzi <parksterac@hotmail.com>
2014-01-25 00:20:06 -0800

    back to working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Franzi::Application.routes.draw do
 
   get "comments/create"
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   resources :posts do
     resources :comments
